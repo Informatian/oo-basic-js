8241204d6f07505d2b2fe1d37587a79aa5deb549
Informatian <434420822@qq.com>
2015-09-27 20:32:06 +0800

    Update main.js

--- practice_v02/src/main.js
+++ practice_v02/src/main.js
@@ -1,1 +1,21 @@
 //  Your code here
+var Person = function (age, name) {
+    this._age = age;
+    this._name = name;
+};Person.prototype.introduce = function () {
+    return "My name is " + this._name + ". I am " + this._age + " years old.";
+};
+var Student = function(name,age,class){
+    Person.call(this,name,age);
+    this.class=class;
+}
+Student.prototype=Object.create(Person.prototype);
+Student.prototype.constructor=Student;
+
+Student.prototype.which_class=function(){
+    console.log("I am a Student"+".I am at Class"+this.class);
+}
+
+var s1=new Student("Tom",21,2);
+s1.how_old();
+s1.which_class();
